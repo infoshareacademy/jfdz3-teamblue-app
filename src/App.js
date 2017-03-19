4b1af22c9177d7bd1eed3ff56e50da7d55e0bf85
Natalia Wiklent <natalia.wiklent@gmail.com>
2017-03-19 17:52:02 +0100

    dodanie sekcji features, favikonki aaand tytul strony

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import Features from './PageLayouts/features/features';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css" />
         <div className="App-header">
@@ -15,9 +18,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+          <Features/>
       </div>
+
+
     )
   }
 }
+
+
 
 export default App;
--- src/PageLayouts/features/features.js
+++ src/PageLayouts/features/features.js
@@ -1,3 +1,35 @@
-/**
- * Created by trener on 05.03.17.
- */
+import React from 'react';
+import FaMobile from 'react-icons/lib/fa/mobile';
+import FaBlind from 'react-icons/lib/fa/blind'
+import FaSearch from 'react-icons/lib/fa/search';
+import FaLock from 'react-icons/lib/fa/lock';
+
+class Features extends React.Component {
+
+
+    render() {
+
+
+
+        return (
+
+            <div className="features">
+
+               <div className="feature-item"><FaMobile /><br/><div className="p-background"><p>Urządzenia mobilne</p></div></div>
+               <div className="feature-item"><FaBlind /><br/><p>Standardy WCAG 2.0</p></div>
+               <div className="feature-item"><FaSearch /><br/><p>Wyszukiwarka produktów</p></div>
+               <div className="feature-item"><FaLock /><br/><p>Bezpieczne zakupy</p></div>
+
+            </div>
+
+
+
+
+        )
+    }
+
+}
+
+
+export default Features;
+

58daa2ae163bbf8bda5dd64982ce9655cb488d22
Olga Kwiatkowska <olgamkwiatkowska@gmail.com>
2017-04-02 20:31:52 +0200

    logowanie SM

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ import Login from './PageLayouts/login/login';
 class App extends Component {
   render() {
     return (
-      < div className="App">
-
+      <div className="App">
+<div id="googleButton"></div>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css" />
         <div className="App-header container-fluid">
--- src/PageLayouts/login/login.js
+++ src/PageLayouts/login/login.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+import FontAwesome from 'react-fontawesome'
+
+const responseGoogle = (response) => {
+    console.log(response);
+}
 
 class Login extends React.Component {
 
 
     render() {
-
-        return (
-
-            <div className="login-box col-md-3" >
-
-               <p>Zaloguj się</p>
-
-            </div>
-
-        )
+        return <div>
+            <GoogleLogin
+                clientId={'763009107746-djupr31f04g6mrdvi2o25f2l7p1eb7n6.apps.googleusercontent.com'}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                offline={false}
+            >
+                <FontAwesome
+                name='google'
+                />
+                <span> Login with Google</span>
+            </GoogleLogin>
+        </div>
     }
-
 }
+
 
 export default Login;
 

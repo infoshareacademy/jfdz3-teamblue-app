73e910c06c5e51985881e802005265db941f381d
Olga Kwiatkowska <olgamkwiatkowska@gmail.com>
2017-04-09 17:25:56 +0200

    wylogowanie SM

--- src/PageLayouts/login/login.js
+++ src/PageLayouts/login/login.js
@@ -3,34 +3,51 @@ import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
 import FontAwesome from 'react-fontawesome'
 
-const responseGoogle = (response) => {
-    console.log(response);
-}
 
 class Login extends React.Component {
 
 
+    responseGoogle = (response) => {
+        localStorage.setItem('loggedUser', JSON.stringify(response));
+        this.setState({
+            loggedUser: response
+        });
+    }
+
+    logoutUser = () => {
+        localStorage.removeItem('loggedUser');
+        this.setState({
+            loggedUser: null
+        });
+    }
 
     render() {
-        return (
+        let user = localStorage.getItem('loggedUser');
+        if( user === null ) {
+            return (
 
-        <div className="google-login">
-            <GoogleLogin
-                clientId={'763009107746-djupr31f04g6mrdvi2o25f2l7p1eb7n6.apps.googleusercontent.com'}
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                offline={false}
-            >
-                <FontAwesome
-                name='google'
-                />
-                <span> Zaloguj się z Google</span>
-            </GoogleLogin>
-        </div>
-        )
+                <div className="google-login">
+                    <GoogleLogin
+                        clientId={'763009107746-djupr31f04g6mrdvi2o25f2l7p1eb7n6.apps.googleusercontent.com'}
+                        onSuccess={this.responseGoogle}
+                        onFailure={this.responseGoogle}
+                        offline={false}
+                    >
+                        <FontAwesome
+                            name='google'
+                        />
+                        <span> Zaloguj się z Google</span>
+                    </GoogleLogin>
+                </div>
+            )
+        }else{
+            user = JSON.parse( user );
+            return (
+                <a href="javascript:void(0)" onClick={ ()=> this.logoutUser() }>Witaj {user.profileObj.name} > Wyloguj</a>
+            )
+
+        }
     }
 }
-
-
 export default Login;
 
